2f227a843f9185a490197c49ff51215e11716b77
Mehedi Hasan <44167334+ProgrammerHasan@users.noreply.github.com>
2025-12-06 11:35:46 +0600

    feat: helpers

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -1,17 +1,3 @@
 <?php
 
-use App\Models\User;
-
-if (!function_exists('authUser')) {
-    function authUser(): User
-    {
-        return auth()->user();
-    }
-}
-
-if (!function_exists('authUserId')) {
-    function authUserId(): ?int
-    {
-        return authUser()?->id;
-    }
-}
+require "auth.php";
